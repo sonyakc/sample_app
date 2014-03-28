161fdc925715c9184055d10ebb9213e467e17bc3
Sonya <sonyakc.2007@gmail.com>
2014-03-27 20:30:23 -0400

    Added additional user and user pages tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
+      flash[:failure] = "Failed to create user"
       render 'new'
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,10 +16,12 @@ class User < ActiveRecord::Base
 
   # before_save { |user| user.email = email.downcase }
   before_save { email.downcase! }
-  
+
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence:true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  
+  after_validation { self.errors.messages.delete(:password_digest) }
 end
